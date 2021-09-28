2c8437ec25534c2ca53fcf88d8340c68deab91c0
simonrahtz <simonrahtz@gmail.com>
2021-09-28 22:55:20 +1000

    DB publishes to web via Json file

---
+++ CarCollabRestAPI/CarCollabRestAPI/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using CarCollabRestAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarCollabRestAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+     
+
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+           using (var context = new CarCollabDbContext())
+            {
+                return context.Persons.ToList();
+            }
+        }
+    }
+}
