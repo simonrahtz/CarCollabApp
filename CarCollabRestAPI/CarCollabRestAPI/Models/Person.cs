c053d065f9efe96b163e1b8d68cf91373610ec6d
simonrahtz <simonrahtz@gmail.com>
2021-09-29 00:38:19 +1000

    Read SQL DB and publish Json file

---
+++ CarCollabRestAPI/CarCollabRestAPI/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CarCollabRestAPI.Models
+{
+    public partial class Person
+    {
+        public int? PersonId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
