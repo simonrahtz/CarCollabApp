bf9a955ecd2d5ccc0f830c1f3b1a899df9c0238b
Simon <simonrahtz@gmail.com>
2021-10-25 14:51:10 +1100

    browser fragment displays list of hardcoded cars

--- ShareCar/app/src/main/java/com/test/sharecar/fragments/BrowseFragment.kt
+++ ShareCar/app/src/main/java/com/test/sharecar/fragments/BrowseFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.test.sharecar.R
 import com.test.sharecar.databinding.FragmentBrowseBinding
 
 
@@ -21,6 +22,12 @@ class BrowseFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentBrowseBinding.inflate(layoutInflater)
+
+        //imageViews display pre loaded images from resource folder
+        binding.car1Image.setImageResource(R.drawable.mazda_626)
+        binding.car2Image.setImageResource(R.drawable.jeep)
+        binding.car3Image.setImageResource(R.drawable.honda_integra)
+
         return binding.root
     }
 
