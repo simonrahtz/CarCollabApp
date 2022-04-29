b38d47ffe1c5cb4293bf7921d62998c0c2bb9b97
Simon <simonrahtz@gmail.com>
2022-04-29 21:50:47 +1000

    Modified user screen

--- ShareCar/app/src/main/java/com/test/sharecar/presentation/bottomnavigation/User.kt
+++ ShareCar/app/src/main/java/com/test/sharecar/presentation/bottomnavigation/User.kt
@@ -6,13 +6,18 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -20,74 +25,49 @@ import com.test.sharecar.R
 
 @Composable
 fun UserScreen() {
+
+    val username = "Username"
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(modifier = Modifier.height(48.dp))
-        Image(
-            painter = painterResource(id = R.drawable.ic_user),
-            contentDescription = "avatar",
-            modifier = Modifier
-                .height(120.dp)
-                .width(120.dp)
-                .clip(shape = RoundedCornerShape(50))
-                .background(Color.LightGray)
-        )
-        Spacer(modifier = Modifier.height(6.dp))
-        Text(
-            text = "username",
-            color = Color.Black,
-            fontSize = 18.sp
-        )
-        Spacer(modifier = Modifier.height(6.dp))
-        Table()
-    }
-}
-
-@Composable
-fun Table() {
-    Column(modifier = Modifier.fillMaxWidth()) {
-        Spacer(
-            modifier = Modifier
-                .height(1.dp)
-                .fillMaxWidth()
-                .background(Color.DarkGray)
-        )
-        Row() {
-            Text(
-                text = "Cars",
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_user),
+                contentDescription = "avatar",
                 modifier = Modifier
-                    .weight(1f)
-                    .padding(6.dp),
-                fontSize = 16.sp
+                    .height(80.dp)
+                    .width(80.dp)
+                    .clip(shape = RoundedCornerShape(50))
+                    .background(Color.LightGray)
             )
-            Spacer(modifier = Modifier.weight(3f))
-            Text(
-                text = "Bookings",
+            Text(text = username,
+                modifier = Modifier.padding(20.dp),
+                style = MaterialTheme.typography.h5
+            )
+            Spacer(Modifier.weight(1f))
+            Text(text = "Manage \nUsers",
                 modifier = Modifier
-                    .weight(1f)
-                    .padding(6.dp),
-                fontSize = 16.sp
+                    .padding(10.dp),
+
+                style = MaterialTheme.typography.body2
             )
         }
-        Spacer(
-            modifier = Modifier
-                .height(1.dp)
-                .fillMaxWidth()
-                .background(Color.DarkGray)
-        )
+        Divider(modifier = Modifier
+            .background(Color.LightGray))
 
-        val scrollState = rememberLazyListState()
-        LazyColumn(state = scrollState) {
-
-        }
     }
 }
 
 
+
+
 @Composable
-@Preview
+@Preview(showBackground = true)
 fun UserScreenPreview() {
     UserScreen()
 }
